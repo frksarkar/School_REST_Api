f370003df2b86c4cbbc980220b30fe3f4446d5b7
omar faruk sarkar <hazrat.omurfaruk@gmail.com>
2023-12-28 01:48:43 +0600

    admin schema

---
+++ module/staff/admin.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const adminSchema = new mongoose.Schema(
+	{
+		name: { type: String, required: true },
+		email: { type: String, required: true },
+		password: { type: String, required: true },
+		role: { type: String, default: 'admin' },
+	},
+	{
+		timestamps: true,
+	}
+);
+
+exports.adminSchema = mongoose.model('Admin', adminSchema);
