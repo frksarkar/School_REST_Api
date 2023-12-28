ff114da859fd426c054f05bfdb4a947d2230b238
omar faruk sarkar <hazrat.omurfaruk@gmail.com>
2023-12-28 18:25:01 +0600

    add academic year schema

---
+++ module/academic/academicYear.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose;
+
+const AcademicYear = new Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		fromYear: {
+			type: Date,
+			required: true,
+		},
+		toYear: {
+			type: Date,
+			required: true,
+		},
+		isCurrent: {
+			type: Boolean,
+			default: false,
+		},
+		createdBy: {
+			type: Schema.Types.ObjectId,
+			ref: 'Admin',
+			required: true,
+		},
+		students: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: 'Student',
+			},
+		],
+		teachers: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: 'Teacher',
+			},
+		],
+	},
+	{
+		timestamps: true,
+	}
+);
+
+exports.AcademicYear = AcademicYear;
