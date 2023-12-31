145caed127fe237b6902e47852827dfeff513cd7
omar faruk sarkar <hazrat.omurfaruk@gmail.com>
2023-12-31 22:35:19 +0600

    error handler

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,16 @@
+exports.errorHandler = function (err, req, res, next) {
+	const stack = err.stark;
+	const message = err.message;
+	const status = err.status || 'failed';
+	const statusCode = err.statusCode || 500;
+
+	res.status(statusCode).json({
+		status,
+		message,
+		stack,
+	});
+};
+
+exports.notFound = function (req, res, next) {
+	res.status(404).json({ message: 'page not found' });
+};
