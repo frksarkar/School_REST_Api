2de1b453ea63641ac9a5d1e34195e781d623c835
omar faruk sarkar <hazrat.omurfaruk@gmail.com>
2023-12-31 22:34:24 +0600

    add error & not found middleware

--- app/app.js
+++ app/app.js
@@ -2,13 +2,22 @@ const morgan = require('morgan');
 const express = require('express');
 
 const { adminRouter } = require('../routes/staff/adminRouter');
+const { errorHandler, notFound } = require('../middlewares/errorHandler');
 
 const app = express();
 
- app.use(morgan('dev'));
+// add middleware
+app.use(morgan('dev'));
+app.use(express.json()); // incoming request body
 
 // Router middleware
 //admin routes
 app.use('/api/v1/admins', adminRouter);
+
+// not found
+app.use(notFound);
+
+// error handlers
+app.use(errorHandler);
 
 exports.app = app;
