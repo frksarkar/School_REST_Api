0f3fd947b11d474117226120d5521447597d8ea7
omar faruk sarkar <hazrat.omurfaruk@gmail.com>
2023-12-31 22:34:48 +0600

    add default value

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+const { defaultValue } = require('../defaultValue');
 
 const dbConnection = async (cb) => {
 	try {
-		await mongoose.connect(process.env.MONGODB_URL);
+		await mongoose.connect(process.env.MONGODB_URL || defaultValue.TestDatabase);
 		console.log('Connected to MongoDB');
 		cb();
 	} catch (err) {
--- server.js
+++ server.js
@@ -4,10 +4,10 @@ require('dotenv').config();
 
 const { app } = require('./app/app');
 const { dbConnect } = require('./config/dbConnection');
+const { defaultValue } = require('./defaultValue');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || defaultValue.PORT;
 const server = http.createServer(app);
-
 dbConnect(() => {
 	server.listen(PORT, (result) => {
 		console.log('server listening on port ', PORT);
