2fbb1449ad4b4ecbadf9e8831b5ad0c2ca9c4a0a
omar faruk sarkar <hazrat.omurfaruk@gmail.com>
2024-01-01 22:36:11 +0600

    auth logic

--- middlewares/authHandler.js
+++ middlewares/authHandler.js
@@ -1,13 +1,33 @@
 const { verifyToken } = require('../utils/helper');
+const { Admin } = require('../module/staff/admin');
 
 exports.isLoggedIn = function (req, res, next) {
-	const token = req.headers.authorization.split(' ')[1];
+	// get token from header
+	const token = req.headers?.authorization?.split(' ')[1];
+
+	// token verify
 	const isValidUser = verifyToken(token);
 	if (!isValidUser) {
 		const error = new Error('invalid authorization token');
 		error.statusCode = 401;
+		``;
 		next(error);
 	}
+
+	// save the token into req.obj
 	req.user = isValidUser;
+	next();
+};
+
+exports.isAdmin = async function (req, res, next) {
+	// find the user in the database
+	const userId = req.user._id;
+	const adminData = await Admin.findOne(userId).select('-password');
+
+	// check if the user is exist then send the user into req.obj
+	if (!adminData?.role === 'admin') {
+		next('Access denied, admin can access only');
+	}
+	req.user = adminData;
 	next();
 };
