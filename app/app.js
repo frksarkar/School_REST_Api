6725b7ec7838db5af239f451195a10f4640ebd86
omar faruk sarkar <hazrat.omurfaruk@gmail.com>
2024-01-03 23:52:12 +0600

    add academic router

--- app/app.js
+++ app/app.js
@@ -4,6 +4,10 @@ const express = require('express');
 const { adminRouter } = require('../routes/staff/adminRouter');
 const { errorHandler, notFound } = require('../middlewares/errorHandler');
 const { academicYearRouter } = require('../routes/academic/academicYearRouter');
+const {
+	academicTermRouter,
+} = require('../routes/academic/academicTermRouter ');
+const { classLevelRouter } = require('../routes/academic/classLevel');
 
 const app = express();
 
@@ -14,7 +18,9 @@ app.use(express.json()); // incoming request body
 // Router middleware
 //admin routes
 app.use('/api/v1/admins', adminRouter);
-app.use('/api/v1/academic-years', academicYearRouter)
+app.use('/api/v1/academic-years', academicYearRouter);
+app.use('/api/v1/academic-terms', academicTermRouter);
+app.use('/api/v1/class-levels', classLevelRouter);
 
 // not found
 app.use(notFound);
