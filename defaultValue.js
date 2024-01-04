83cf9e713bf35310d861c918ce32f1cde70e9d17
omar faruk sarkar <hazrat.omurfaruk@gmail.com>
2024-01-04 23:03:41 +0600

    default variable setting

--- defaultValue.js
+++ defaultValue.js
@@ -1,6 +1,16 @@
 exports.defaultValue = {
 	jwtSecret: 'LMSjwtPrivateKey',
-	jwtExpiration: '1h',
+	jwtExpiration: '1d',
 	PORT: 3000,
 	TestDatabase: 'demo link',
+};
+
+exports.defaultAppValue = {
+	programDuration: '4 years',
+	academicTermDuration: '3 months',
+	subjectDuration: '3 months',
+	examDuration: '30 minutes',
+	examResultEnumVal: ['Excellent', 'Good', 'Poor'],
+	examStatusEnumVal: ['failed', 'passed'],
+	teacherApplicationStatusEnumVal: ['pending', 'approved', 'rejected'],
 };
