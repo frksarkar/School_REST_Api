499ed6c6ae13a5921136edc0a1c635cc1571c0b8
omar faruk sarkar <hazrat.omurfaruk@gmail.com>
2024-01-06 21:20:11 +0600

    add router

--- app/app.js
+++ app/app.js
@@ -2,14 +2,14 @@ const morgan = require('morgan');
 const express = require('express');
 
 const { adminRouter } = require('../routes/staff/adminRouter');
-const { errorHandler, notFound } = require('../middlewares/errorHandler');
-const { academicYearRouter } = require('../routes/academic/academicYearRouter');
-const {
-	academicTermRouter,
-} = require('../routes/academic/academicTermRouter ');
-const { classLevelRouter } = require('../routes/academic/classLevelRouter');
+const { teacherRouter } = require('../routes/staff/teacherRouter');
 const { programRouter } = require('../routes/academic/programRouter');
 const { subjectRouter } = require('../routes/academic/subjectRouter');
+const { errorHandler, notFound } = require('../middlewares/errorHandler');
+const { yearGroupRouter } = require('../routes/academic/yearGroupRouter');
+const { classLevelRouter } = require('../routes/academic/classLevelRouter');
+const { academicYearRouter } = require('../routes/academic/academicYearRouter');
+const { academicTermRouter } = require('../routes/academic/academicTermRouter ');
 
 const app = express();
 
@@ -25,6 +25,8 @@ app.use('/api/v1/academic-terms', academicTermRouter);
 app.use('/api/v1/class-levels', classLevelRouter);
 app.use('/api/v1/programs', programRouter);
 app.use('/api/v1/subjects', subjectRouter);
+app.use('/api/v1/year-groups', yearGroupRouter);
+app.use('/api/v1/teachers', teacherRouter);
 
 // not found
 app.use(notFound);
