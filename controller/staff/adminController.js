ea01537187a953cfbe9013bd84454fd4b4de13bb
omar faruk sarkar <hazrat.omurfaruk@gmail.com>
2024-01-06 21:20:43 +0600

    update admin controller

--- controller/staff/adminController.js
+++ controller/staff/adminController.js
@@ -1,14 +1,19 @@
+const { defaultValue } = require('../../defaultValue');
+const { throwErr } = require('../../middlewares/errorHandler');
 const { Admin } = require('../../module/staff/admin');
-const { tokenGenerate, verifyToken } = require('../../utils/helper');
+const { tokenGenerate } = require('../../utils/helper');
 
 exports.adminRegister = async function (req, res, next) {
 	const { name, email, password } = req.body;
 
 	try {
+		if (!(name && email && password)) {
+			throwErr('all input field are required', 400);
+		}
+
 		const isExist = await Admin.findOne({ email });
 		if (isExist) {
-			res.json({ status: 'account already exists' });
-			return;
+			throwErr('account already exists', 403);
 		}
 		const user = await Admin.create({ name, email, password });
 
@@ -17,35 +22,41 @@ exports.adminRegister = async function (req, res, next) {
 			data: user,
 		});
 	} catch (error) {
-		res.json({ status: error.message });
+		next(error);
 	}
 };
 
 exports.adminLogin = async function (req, res, next) {
 	const { email, password } = req.body;
 	try {
+		if (!(email && password)) {
+			throwErr('email and password are required', 400);
+		}
+
 		const user = await Admin.findOne({ email });
 		if (!user) {
-			res.json({ status: 'user not found' });
-			return;
+			throwErr('user not found', 500);
 		}
+
 		const isVerified = await user.verifyPassword(password);
 
 		if (!isVerified) {
-			res.json({ status: 'password not correct' });
-			return;
+			throwErr('password is not correct', 400);
 		}
 
-		const token = tokenGenerate({
-			id: user._id.toString(),
-			name: user.name,
-			email: user.email,
-		});
+		const token = tokenGenerate(
+			{
+				id: user._id.toString(),
+				name: user.name,
+				email: user.email,
+			},
+			defaultValue.adminJwtExpiration
+		);
 
 		res.status(200).json({
 			status: 'success',
 			message: 'admin login successful',
-			data: token,
+			token: token,
 		});
 	} catch (error) {
 		next(error);
@@ -61,7 +72,7 @@ exports.getAdmins = async function (req, res, next) {
 			data: users,
 		});
 	} catch (error) {
-		console.log(error.message);
+		next(error);
 	}
 };
 
@@ -80,17 +91,34 @@ exports.getAdmin = async function (req, res, next) {
 exports.updateAdmin = async function (req, res, next) {
 	const { name, email, password } = req.body;
 	try {
-		const user = await Admin.findOneAndUpdate(
-			{ _id: req.user._id },
-			{ name, password, email },
-		);
-		console.log('🚀 ~ file: adminController.js:58 ~ user:', user);
-
+		// find email and throw error if email already exists
+		const emailExist = await Admin.findOne({ email });
+		if (emailExist) {
+			throwErr('email already exists', 403);
+		}
+		// update the user info
+		if (name || email || password) {
+			const user = await Admin.findById(req.user._id);
+			if (name) {
+				user.name = name;
+			}
+			if (email) {
+				user.email = email;
+			}
+			if (password) {
+				user.password = password;
+			}
+			user.save();
+		} else {
+			throwErr('provided atlas, one field value', 200);
+		}
+		// send back to the response object.
 		res.status(202).json({
+			status: 'success',
 			message: 'update successful',
 		});
 	} catch (error) {
-		console.log(error.message);
+		next(error);
 	}
 };
 
