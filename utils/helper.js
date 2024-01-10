cd6f05d91c4cdec6c3f6b3fcde2ef50ece82571f
omar faruk sarkar <hazrat.omurfaruk@gmail.com>
2024-01-10 21:28:15 +0600

    password encrypt and validate function

--- utils/helper.js
+++ utils/helper.js
@@ -1,9 +1,11 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
 const { defaultValue } = require('../defaultValue');
 
-exports.tokenGenerate = function (data) {
+exports.tokenGenerate = function (data, expireTime) {
 	const secretKey = process.env.JWT_SECRET_KEY || defaultValue.jwtSecret;
-	return jwt.sign(data, secretKey, { expiresIn: defaultValue.jwtExpiration });
+	return jwt.sign(data, secretKey, { expiresIn: expireTime });
 };
 
 exports.verifyToken = function (token) {
@@ -11,6 +13,22 @@ exports.verifyToken = function (token) {
 	try {
 		return jwt.verify(token, secretKey);
 	} catch (error) {
-		return false;
+		return false; 
 	}
 };
+
+exports.passwordEncrypt = async function (next) {
+	if (this.password.length > 30) {
+		next();
+		return;
+	}
+	const passwordHash = await bcrypt.hash(this.password, 12);
+	this.password = passwordHash;
+
+	next();
+};
+
+exports.isValidPassword = function (enterPassword) {
+	return bcrypt.compare(enterPassword, this.password);
+};
+
