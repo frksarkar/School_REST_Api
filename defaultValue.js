229fae6f4c5a277b9203a8479629c138407b0879
omar faruk sarkar <hazrat.omurfaruk@gmail.com>
2024-01-10 21:23:34 +0600

    add default value student jwt expiation time

--- defaultValue.js
+++ defaultValue.js
@@ -2,6 +2,7 @@ exports.defaultValue = {
 	jwtSecret: 'LMSjwtPrivateKey',
 	adminJwtExpiration: '1d',
 	teacherJwtExpiration: '1d',
+	studentJwtExpiration: '1d',
 	PORT: 3000,
 	TestDatabase: 'demo link',
 };
