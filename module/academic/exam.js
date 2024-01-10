b9b71ac030f15263dd770990dcd0255507e3cb3d
omar faruk sarkar <hazrat.omurfaruk@gmail.com>
2024-01-10 21:28:54 +0600

    exam schema model

--- module/academic/exam.js
+++ module/academic/exam.js
@@ -1,89 +1,90 @@
 const mongoose = require('mongoose');
+const { defaultAppValue } = require('../../defaultValue');
 
 const { Schema } = mongoose;
 
 const examSchema = new Schema(
 	{
-        name: {
-            type: String,
-            required: true,
-        },
-        description: {
-            type: String,
-            required: true,
-        },
-        subject: {
-            type: Schema.Types.ObjectId,
-            ref: 'Subject',
-            required: true,
-        },
-        program: {
-            type: Schema.Types.ObjectId,
-            ref: 'Program',
-            required: true,
-        },
-        passMark: {
-            type: Number,
-            required: true,
-            default: 50,
-        },
-        totalMark: {
-            type: Number,
-            required: true,
-            default: 100,
-        },
-        
-        duration: {
-            type: String,
-            required: true,
-            default: '30 minutes'
-        },
-        examDate: {
-            type: Date,
-            required: true,
-        },
-        examTime: {
-            type: String,
-            required: true,
-        },
-        examType: {
-            type: String,
-            required: true,
-            default: 'Quiz',
-        }, 
-        examStatus: {
-            type: String,
-            required: true,
-            default: 'Pending',
-            enum: ['Pending', 'live']
-        }, 
-        question: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'Question',
-            }
-        ],
-        classLevel: {
-            type: Schema.Types.ObjectId,
-            ref: 'ClassLevel',
-            required: true,
-        }, 
-        createdBy: {
-            type: Schema.Types.ObjectId,
-            ref: 'Teacher',
-            required: true,
-        },
-        academicTerm: {
-            type: Schema.Types.ObjectId,
-            ref: 'AcademicTerm',
-            required: true,
-        },
-        academicYear: {
-            type: Schema.Types.ObjectId,
-            ref: 'AcademicYear',
-            required: true,
-        }
-    },
+		name: {
+			type: String,
+			required: true,
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+		subject: {
+			type: Schema.Types.ObjectId,
+			ref: 'Subject',
+			required: true,
+		},
+		program: {
+			type: Schema.Types.ObjectId,
+			ref: 'Program',
+			required: true,
+		},
+		passMark: {
+			type: Number,
+			required: true,
+			default: 33,
+		},
+		totalMark: {
+			type: Number,
+			required: true,
+			default: 100,
+		},
+
+		duration: {
+			type: String,
+			required: true,
+			default: defaultAppValue.examDuration,
+		},
+		examDate: {
+			type: Date,
+			required: true,
+		},
+		examTime: {
+			type: String,
+			required: true,
+		},
+		examType: {
+			type: String,
+			required: true,
+			default: 'Quiz',
+		},
+		examStatus: {
+			type: String,
+			required: true,
+			default: 'pending',
+			enum: ['pending', 'live'],
+		},
+		question: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: 'Question',
+			},
+		],
+		classLevel: {
+			type: Schema.Types.ObjectId,
+			ref: 'ClassLevel',
+			required: true,
+		},
+		createdBy: {
+			type: Schema.Types.ObjectId,
+			ref: 'Teacher',
+			required: true,
+		},
+		academicTerm: {
+			type: Schema.Types.ObjectId,
+			ref: 'AcademicTerm',
+			required: true,
+		},
+		academicYear: {
+			type: Schema.Types.ObjectId,
+			ref: 'AcademicYear',
+			required: true,
+		},
+	},
 	{
 		timestamps: true,
 	}
